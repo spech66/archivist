2256c73436135e389f672328256b44b51ef9e0e2
Sebastian Pech <spech66@users.noreply.github.com>
2012-11-14 11:01:36 +0000

    Empty Deck, Library

---
+++ archivist2/Archivist/Deck.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Archivist
+{
+	public partial class Deck : UserControl
+	{
+		private string deckFilename;
+
+		public Deck(string path = "")
+		{
+			InitializeComponent();
+
+			if (!String.IsNullOrEmpty(path))
+			{
+				deckFilename = path;
+			}
+		}
+	}
+}
--- archivist2/Archivist/UpdateDatabase.Designer.cs
+++ archivist2/Archivist/UpdateDatabase.Designer.cs
@@ -35,7 +35,11 @@ namespace Archivist
 			this.progressBarTaskStatus = new System.Windows.Forms.ProgressBar();
 			this.progressBarStatus = new System.Windows.Forms.ProgressBar();
 			this.button1 = new System.Windows.Forms.Button();
+			this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+			this.btnGatherer = new System.Windows.Forms.Button();
+			this.btnSoftware = new System.Windows.Forms.Button();
 			this.tableLayoutPanel1.SuspendLayout();
+			this.flowLayoutPanel1.SuspendLayout();
 			this.SuspendLayout();
 			// 
 			// tableLayoutPanel1
@@ -48,16 +52,16 @@ namespace Archivist
 			this.tableLayoutPanel1.Controls.Add(this.labelStatus, 0, 2);
 			this.tableLayoutPanel1.Controls.Add(this.progressBarTaskStatus, 1, 1);
 			this.tableLayoutPanel1.Controls.Add(this.progressBarStatus, 1, 2);
-			this.tableLayoutPanel1.Controls.Add(this.button1, 1, 3);
+			this.tableLayoutPanel1.Controls.Add(this.flowLayoutPanel1, 1, 3);
 			this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
 			this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
 			this.tableLayoutPanel1.Name = "tableLayoutPanel1";
 			this.tableLayoutPanel1.RowCount = 4;
 			this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
-			this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 40F));
-			this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 40F));
-			this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 30F));
-			this.tableLayoutPanel1.Size = new System.Drawing.Size(701, 322);
+			this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 28F));
+			this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 31F));
+			this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 35F));
+			this.tableLayoutPanel1.Size = new System.Drawing.Size(701, 330);
 			this.tableLayoutPanel1.TabIndex = 0;
 			// 
 			// listStatus
@@ -67,16 +71,16 @@ namespace Archivist
 			this.listStatus.FormattingEnabled = true;
 			this.listStatus.Location = new System.Drawing.Point(3, 3);
 			this.listStatus.Name = "listStatus";
-			this.listStatus.Size = new System.Drawing.Size(695, 206);
+			this.listStatus.Size = new System.Drawing.Size(695, 230);
 			this.listStatus.TabIndex = 0;
 			// 
 			// labelTaskStatus
 			// 
 			this.labelTaskStatus.AutoSize = true;
 			this.labelTaskStatus.Dock = System.Windows.Forms.DockStyle.Fill;
-			this.labelTaskStatus.Location = new System.Drawing.Point(3, 212);
+			this.labelTaskStatus.Location = new System.Drawing.Point(3, 236);
 			this.labelTaskStatus.Name = "labelTaskStatus";
-			this.labelTaskStatus.Size = new System.Drawing.Size(114, 40);
+			this.labelTaskStatus.Size = new System.Drawing.Size(114, 28);
 			this.labelTaskStatus.TabIndex = 1;
 			this.labelTaskStatus.Text = "Task Staus:";
 			// 
@@ -84,24 +88,24 @@ namespace Archivist
 			// 
 			this.labelStatus.AutoSize = true;
 			this.labelStatus.Dock = System.Windows.Forms.DockStyle.Fill;
-			this.labelStatus.Location = new System.Drawing.Point(3, 252);
+			this.labelStatus.Location = new System.Drawing.Point(3, 264);
 			this.labelStatus.Name = "labelStatus";
-			this.labelStatus.Size = new System.Drawing.Size(114, 40);
+			this.labelStatus.Size = new System.Drawing.Size(114, 31);
 			this.labelStatus.TabIndex = 2;
 			this.labelStatus.Text = "Status";
 			// 
 			// progressBarTaskStatus
 			// 
 			this.progressBarTaskStatus.Dock = System.Windows.Forms.DockStyle.Top;
-			this.progressBarTaskStatus.Location = new System.Drawing.Point(123, 215);
+			this.progressBarTaskStatus.Location = new System.Drawing.Point(123, 239);
 			this.progressBarTaskStatus.Name = "progressBarTaskStatus";
-			this.progressBarTaskStatus.Size = new System.Drawing.Size(575, 23);
+			this.progressBarTaskStatus.Size = new System.Drawing.Size(575, 22);
 			this.progressBarTaskStatus.TabIndex = 3;
 			// 
 			// progressBarStatus
 			// 
 			this.progressBarStatus.Dock = System.Windows.Forms.DockStyle.Top;
-			this.progressBarStatus.Location = new System.Drawing.Point(123, 255);
+			this.progressBarStatus.Location = new System.Drawing.Point(123, 267);
 			this.progressBarStatus.Name = "progressBarStatus";
 			this.progressBarStatus.Size = new System.Drawing.Size(575, 23);
 			this.progressBarStatus.TabIndex = 4;
@@ -110,7 +114,7 @@ namespace Archivist
 			// 
 			this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
 			this.button1.Enabled = false;
-			this.button1.Location = new System.Drawing.Point(623, 295);
+			this.button1.Location = new System.Drawing.Point(497, 3);
 			this.button1.Name = "button1";
 			this.button1.Size = new System.Drawing.Size(75, 23);
 			this.button1.TabIndex = 5;
@@ -118,11 +122,44 @@ namespace Archivist
 			this.button1.UseVisualStyleBackColor = true;
 			this.button1.Click += new System.EventHandler(this.button1_Click);
 			// 
+			// flowLayoutPanel1
+			// 
+			this.flowLayoutPanel1.Controls.Add(this.button1);
+			this.flowLayoutPanel1.Controls.Add(this.btnGatherer);
+			this.flowLayoutPanel1.Controls.Add(this.btnSoftware);
+			this.flowLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.flowLayoutPanel1.FlowDirection = System.Windows.Forms.FlowDirection.RightToLeft;
+			this.flowLayoutPanel1.Location = new System.Drawing.Point(123, 298);
+			this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+			this.flowLayoutPanel1.Size = new System.Drawing.Size(575, 29);
+			this.flowLayoutPanel1.TabIndex = 6;
+			// 
+			// btnGatherer
+			// 
+			this.btnGatherer.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+			this.btnGatherer.Location = new System.Drawing.Point(388, 3);
+			this.btnGatherer.Name = "btnGatherer";
+			this.btnGatherer.Size = new System.Drawing.Size(103, 23);
+			this.btnGatherer.TabIndex = 6;
+			this.btnGatherer.Text = "Gatherer Update";
+			this.btnGatherer.UseVisualStyleBackColor = true;
+			this.btnGatherer.Click += new System.EventHandler(this.btnGatherer_Click);
+			// 
+			// btnSoftware
+			// 
+			this.btnSoftware.Location = new System.Drawing.Point(243, 3);
+			this.btnSoftware.Name = "btnSoftware";
+			this.btnSoftware.Size = new System.Drawing.Size(139, 23);
+			this.btnSoftware.TabIndex = 7;
+			this.btnSoftware.Text = "Software/Cardlist Update";
+			this.btnSoftware.UseVisualStyleBackColor = true;
+			this.btnSoftware.Click += new System.EventHandler(this.btnSoftware_Click);
+			// 
 			// UpdateDatabase
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(701, 322);
+			this.ClientSize = new System.Drawing.Size(701, 330);
 			this.Controls.Add(this.tableLayoutPanel1);
 			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
 			this.MaximizeBox = false;
@@ -133,6 +170,7 @@ namespace Archivist
 			this.Load += new System.EventHandler(this.UpdateDatabase_Load);
 			this.tableLayoutPanel1.ResumeLayout(false);
 			this.tableLayoutPanel1.PerformLayout();
+			this.flowLayoutPanel1.ResumeLayout(false);
 			this.ResumeLayout(false);
 
         }
@@ -146,5 +184,8 @@ namespace Archivist
         private System.Windows.Forms.ProgressBar progressBarTaskStatus;
         private System.Windows.Forms.ProgressBar progressBarStatus;
         private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+		private System.Windows.Forms.Button btnGatherer;
+		private System.Windows.Forms.Button btnSoftware;
     }
 }
