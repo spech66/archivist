07f79fe2d289cd4ecc611f4b59ad8c776652d9d0
Sebastian Pech <spech66@users.noreply.github.com>
2012-11-17 00:09:52 +0000

    Draw simulation

--- archivist2/Archivist/Global.cs
+++ archivist2/Archivist/Global.cs
@@ -2,11 +2,33 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Security.Cryptography;
 
 namespace Archivist
 {
-	public class Global
-	{
+	public static class Global
+    {
+        /// <summary>
+        /// Shuffle using http://en.wikipedia.org/wiki/Fisher-Yates_shuffle
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="list"></param>
+        public static void Shuffle<T>(this IList<T> list)
+        {
+            RNGCryptoServiceProvider provider = new RNGCryptoServiceProvider();
+            int n = list.Count;
+            while (n > 1)
+            {
+                byte[] box = new byte[1];
+                do provider.GetBytes(box);
+                while (!(box[0] < n * (Byte.MaxValue / n)));
+                int k = (box[0] % n);
+                n--;
+                T value = list[k];
+                list[k] = list[n];
+                list[n] = value;
+            }
+        }
 	}
 
 	public class ListBoxItemNameId
